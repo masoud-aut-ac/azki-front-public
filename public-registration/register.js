1e970eda2e8b655b9e97beec989f389ae786028c
Reyhaneh Safaie <rsafaie7899@gmail.com>
2025-08-28 14:49:27 +0330

    edited:1

--- public-registration/register.js
+++ public-registration/register.js
@@ -7,7 +7,6 @@ async function registerBtn() {
     r.nationalCode = document.getElementById('nationalCode').value.trim();
     r.refererCode = document.getElementById('refererCode').value.trim();
     r.frontImage = document.getElementById('frontImage').value.trim();
-    r.backImage = document.getElementById('backImage').value.trim();
 
 
     if (!r.fullName || !r.phone || !r.nationalCode) {
@@ -15,11 +14,20 @@ async function registerBtn() {
         return;
     }
 
-    if (r.phone.length !== 11 || !r.phone.startsWith("09")) {
+    let convertResult = convertFarsiToEnglishNums(r.phone);
+    if (r.phone.length !== 11  || convertResult.hasError || !convertResult.converted.startsWith("09")) {
         showResult("شماره همراه اشتباه است!");
         return;
     }
+    r.phone = convertResult.converted;
 
+
+    let convertIdResult = convertFarsiToEnglishNums(r.nationalCode);
+    if (r.nationalCode.length !== 10 || convertIdResult.hasError) {
+        showResult("کد ملی اشتباه است!");
+        return;
+    }
+    r.nationalCode = convertIdResult.converted;
 
     const registerResponse = await register(r);
 
@@ -33,6 +41,35 @@ async function registerBtn() {
         showResult("خطا در برقراری ارتباط ، لطفا بعدا تلاش کنید ")
     }
 }
+
+let numbersMap = {"۰": 0, "۱": 1, "۲": 2, "۳": 3, "۴": 4, "۵": 5, "۶": 6, "۷": 7, "۸": 8, "۹": 9,};
+
+function convertFarsiToEnglishNums(n) {
+    let result = [];
+    let hasError = false;
+
+    for (let digit of n) {
+        if (containsOnlyDigits(digit)) {
+            result.push(digit);
+            continue;
+        }
+        if (numbersMap[digit] !== undefined) {
+            result.push(numbersMap[digit]);
+            continue;
+        }
+        hasError = true;
+    }
+
+    return {
+        converted: result.join(''),
+        hasError: hasError,
+    };
+}
+
+function containsOnlyDigits(str) {
+    return /^\d+$/.test(str);
+}
+
 
 export function showResult(message) {
     const errorDiv = document.getElementById("errorDiv");
